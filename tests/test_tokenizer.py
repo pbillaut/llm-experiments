67f718c80830eb05f39e56382d27baefc825259d
Patrice Billaut <57354406+pbillaut@users.noreply.github.com>
2025-05-24 22:18:54 +0200

    chore: fix formatting (#2)

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -6,7 +6,7 @@ from llm.tokenizer import SimpleTokenizerV1, Vocab, generate_vocab, naive_tokeni
 
 
 def test_naive_tokenizer_the_verdict(resource: Path):
-    path = resource / 'the-verdict.txt'
+    path = resource / "the-verdict.txt"
     raw_text = path.read_text()
     tokens = naive_tokenization(raw_text)
     assert len(tokens) == 4690
@@ -17,7 +17,17 @@ def test_generate_vocabulary():
     vocab_tokens = naive_tokenization(text)
     vocab = generate_vocab(vocab_tokens)
 
-    assert vocab == {'The': 0, 'brown': 1, 'dog': 2, 'fox': 3, 'jumps': 4, 'lazy': 5, 'over': 6, 'quick': 7, 'the': 8}
+    assert vocab == {
+        "The": 0,
+        "brown": 1,
+        "dog": 2,
+        "fox": 3,
+        "jumps": 4,
+        "lazy": 5,
+        "over": 6,
+        "quick": 7,
+        "the": 8,
+    }
 
 
 class TestSimpleTokenizerV1:
@@ -36,7 +46,7 @@ class TestSimpleTokenizerV1:
 
     def test_encode_fails_for_token_not_in_vocab(self, vocab: Vocab):
         tokenizer = SimpleTokenizerV1(vocab)
-        unknown_token = 'elephant'
+        unknown_token = "elephant"
         with pytest.raises(KeyError, match=unknown_token):
             tokenizer.encode(f"The quick brown {unknown_token} jumps over the lazy dog")
 
