2c76392a737bfce8cea36501d81b776e5c63c1ec
Patrice Billaut <billaut@proton.me>
2025-05-24 21:47:20 +0200

    feat: add simple tokenizer

--- llm/tokenizer.py
+++ llm/tokenizer.py
@@ -1,8 +1,38 @@
 import re
+from typing import TypeAlias
+
+Vocab: TypeAlias = dict[str, int]
 
 NAIVE_TOKENS = re.compile(r'''([_,.;:?!"'()]|--|\s)''')
+
+
+def generate_vocab(tokens: list[str]) -> Vocab:
+    all_words = sorted(set(tokens))
+    return {token: token_id for token_id, token in enumerate(all_words)}
 
 
 def naive_tokenization(text: str) -> list[str]:
     tokens = NAIVE_TOKENS.split(text)
     return [text.strip() for text in tokens if text.strip()]
+
+
+DECODE_PATTERN = re.compile(r'''\s+([,.?!"'()])''')
+
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab: Vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i: s for s, i in vocab.items()}
+
+    def encode(self, text: str) -> list[int]:
+        pre_processed = naive_tokenization(text)
+        try:
+            token_ids = [self.str_to_int[s] for s in pre_processed]
+        except KeyError as e:
+            raise KeyError(f"Unknown token: {e}")
+        return token_ids
+
+    def decode(self, token_ids: list[int]) -> str:
+        text = " ".join([self.int_to_str[i] for i in token_ids])
+        text = DECODE_PATTERN.sub(r'\1', text)
+        return text
--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
-from llm.tokenizer import naive_tokenization
+import pytest
+
+from llm.tokenizer import SimpleTokenizerV1, Vocab, generate_vocab, naive_tokenization
 
 
 def test_naive_tokenizer_the_verdict(resource: Path):
@@ -8,3 +10,38 @@ def test_naive_tokenizer_the_verdict(resource: Path):
     raw_text = path.read_text()
     tokens = naive_tokenization(raw_text)
     assert len(tokens) == 4690
+
+
+def test_generate_vocabulary():
+    text = "The quick brown fox jumps over the lazy dog"
+    vocab_tokens = naive_tokenization(text)
+    vocab = generate_vocab(vocab_tokens)
+
+    assert vocab == {'The': 0, 'brown': 1, 'dog': 2, 'fox': 3, 'jumps': 4, 'lazy': 5, 'over': 6, 'quick': 7, 'the': 8}
+
+
+class TestSimpleTokenizerV1:
+    TEXT = "The quick brown fox jumps over the lazy dog"
+
+    @pytest.fixture()
+    def vocab(self) -> Vocab:
+        vocab_tokens = naive_tokenization(self.TEXT)
+        return generate_vocab(vocab_tokens)
+
+    def test_encode(self, vocab: Vocab):
+        tokenizer = SimpleTokenizerV1(vocab)
+        encoded = tokenizer.encode(self.TEXT)
+
+        assert encoded == [0, 7, 1, 3, 4, 6, 8, 5, 2]
+
+    def test_encode_fails_for_token_not_in_vocab(self, vocab: Vocab):
+        tokenizer = SimpleTokenizerV1(vocab)
+        unknown_token = 'elephant'
+        with pytest.raises(KeyError, match=unknown_token):
+            tokenizer.encode(f"The quick brown {unknown_token} jumps over the lazy dog")
+
+    def test_decode(self, vocab: Vocab):
+        tokenizer = SimpleTokenizerV1(vocab)
+        decoded = tokenizer.decode([0, 7, 1, 3, 4, 6, 8, 5, 2])
+
+        assert decoded == self.TEXT
